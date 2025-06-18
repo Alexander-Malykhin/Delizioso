d4bdd692f64b7c4a1cf042bb3c81c1bd5c3ed31c
Alexander Malykhin <well02091999@gmail.com>
2025-06-19 00:56:29 +0300

    add header

---
+++ src/components/navigation/settings/navigatonList.js
@@ -0,0 +1,27 @@
+export const navigationList = [
+    {
+        id: 1,
+        name: 'Home',
+        path: '/',
+    },
+    {
+        id: 2,
+        name: 'About us',
+        path: '/about',
+    },
+    {
+        id: 3,
+        name: 'Order online',
+        path: '/order',
+    },
+    {
+        id: 4,
+        name: 'Reservation',
+        path: '/reservation',
+    },
+    {
+        id: 5,
+        name: 'Contact us',
+        path: '/contact',
+    },
+]
